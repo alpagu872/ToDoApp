a888c1f72b1c0fceac0229951688bcad71b295d3
alpagu872 <83309872+alpagu872@users.noreply.github.com>
2022-02-16 02:13:16 +0300

    kod inceledim |01:36- 02:13

--- src/App.js
+++ src/App.js
@@ -12,20 +12,48 @@ import {
   View,
 } from 'react-native';
 
-function App(){
-  return(
+function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.top_container}>
+        <Text style={styles.title}>Yapılacaklar</Text>
+        <View style={styles.counter}><Text >0</Text></View>
 
-    <View>
-      <Text>Hii</Text>
-    </View>
+      </View>
+      <View>
+        <Text>Hii</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+  container: {
+    flex: 1,
+    backgroundColor: '#4a4a4a'
+
+  },
+  top_container: {
+ 
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    
+
+
+  },
+
+  title: {
+    fontSize: 36,
+    color: 'orange',
+    padding: 12
+  },
+  counter: {
+    justifyContent: 'flex-end',
+    
+    
+
   }
 
 });
